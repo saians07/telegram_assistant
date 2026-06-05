141934b653fc52b1903428087ce2688ffad29e56
saians07 <suberlinsinaga@gmail.com>
2026-06-06 00:49:39 +0700

    Move owner chat id into first data to be checked

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,45 @@
-use std::env;
-use teloxide::{prelude::*, types::InputFile};
+use axum::{Json, Router, extract::State, http::StatusCode, routing::post, serve};
+use std::{env, net::SocketAddr};
+use teloxide::prelude::*;
+
+use bella_swan::state::{self, AppState};
 
 #[tokio::main]
 async fn main() {
     let bot = teloxide::Bot::from_env();
-
-    let id = env::var("OWNER_CHAT_ID")
+    let owner_chat_id = env::var("OWNER_CHAT_ID")
         .unwrap_or_default()
         .parse::<i64>()
         .unwrap_or_default();
+    let app_state = state::AppState::new(bot, owner_chat_id);
 
-    teloxide::repl(bot.clone(), move |bot: Bot, msg: Message| async move {
-        if msg.chat.id == ChatId(id) {
-            match msg.text() {
-                Some(text) => {
-                    let _ = bot
-                        .send_message(msg.chat.id, format!("Kamu kirim pesan text! {}", text))
-                        .await;
-                }
-                _ => {}
-            }
+    // defining the listener that will listen to our TCP
+    // Deliberately pun panics here when the app start to ensure whoever starts
+    // this server will get notified.
+    let address = SocketAddr::from(([0, 0, 0, 0], 8300));
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
 
-            bot.send_dice(msg.chat.id).await?;
-        } else {
-            let audio_file = InputFile::file("assets/voices/Belum_Terdaftar.mp3");
-            let _ = bot
-                .send_voice(msg.chat.id, audio_file)
-                .caption("Pesan dari Tarzan!")
-                .await?;
-        }
+    let app: Router = Router::new()
+        .route("/webhook", post(telegram_handler))
+        .with_state(app_state.clone());
 
-        Ok(())
-    })
-    .await;
+    serve(listener, app).await.unwrap();
+}
+
+async fn telegram_handler(
+    State(state): State<AppState>,
+    Json(update): Json<Update>,
+) -> Result<axum::http::StatusCode, ()> {
+    println!("You got a new messages: {:#?}", update);
+    let Some(message) = update.chat() else {
+        return Ok(StatusCode::BAD_REQUEST);
+    };
+    let a = state
+        .bot
+        .send_message(message.id, "You are sending me a message")
+        .await
+        .map_err(|e| print!("Error: {:#?}", e))?;
+
+    print!("{:#?}", a);
+    Ok(axum::http::StatusCode::OK)
 }
