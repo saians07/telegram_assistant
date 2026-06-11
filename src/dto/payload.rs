066cfe7771eb3d2269f8a261f8d508f48ae768a1
saians07 <suberlinsinaga@gmail.com>
2026-06-11 09:05:02 +0700

    Add method to convert url string into Url object.

--- src/dto/payload.rs
+++ src/dto/payload.rs
@@ -1,13 +1,17 @@
 use reqwest::Url;
 use serde::{Deserialize, Serialize};
 
+use crate::core::error::SwanError;
+
 #[derive(Serialize, Deserialize, utoipa::ToSchema, Debug)]
 pub struct NewWebhook {
     pub webhook_url: String,
 }
 
 impl NewWebhook {
-    pub fn into_url(&self) -> Result<Url, ()> {
-        Url::parse(&self.webhook_url).map_err(|_| ())
+    pub fn into_url(&self) -> Result<Url, SwanError> {
+        Url::parse(&self.webhook_url).map_err(|e| {
+            SwanError::operation_with_source("Failed to convert new webhook URL to a URL type!", e)
+        })
     }
 }
