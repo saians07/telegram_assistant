c711ca9d96275d268caa4d1c31d45e4c6bb9e5a0
saians07 <suberlinsinaga@gmail.com>
2026-06-11 09:04:32 +0700

    Integrate logging into our system

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use axum::{Router, serve};
 use std::{env, net::SocketAddr};
+use tower::ServiceBuilder;
 
 use swan::{
     api::routes::create_routes,
+    core::{log::swan_tracing, middleware::request_id_generator::RequestLayer},
     state::{self},
 };
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+    swan_tracing();
     let bot_token = env::var("TELEGRAM_BOT_TOKEN").unwrap_or_default();
     let owner_chat_id = env::var("TBOT_OWNER_ID")
         .unwrap_or_default()
@@ -24,9 +27,13 @@ async fn main() {
     let address = SocketAddr::from(([0, 0, 0, 0], 8300));
     let listener = tokio::net::TcpListener::bind(address).await.unwrap();
 
+    let tower = ServiceBuilder::new().layer(RequestLayer);
+
     let app = Router::new()
         .merge(backend_api_routes)
+        .layer(tower)
         .with_state(app_state.clone());
 
+    tracing::info!("Starting server on {}", address);
     serve(listener, app).await.unwrap();
 }
