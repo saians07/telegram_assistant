df0417a685acd319a0a193de4aa00f35fce1ac2d
saians07 <suberlinsinaga@gmail.com>
2026-06-11 14:23:50 +0700

    Convert the state to be Arc data.

--- src/state.rs
+++ src/state.rs
@@ -1,8 +1,12 @@
+use std::{ops::Deref, sync::Arc};
+
 use secrecy::SecretString;
 use teloxide::Bot;
 
 #[derive(Clone)]
-pub struct AppState {
+pub struct AppState(Arc<AppStateInner>);
+
+pub struct AppStateInner {
     pub bot: Bot, // teloxide bot has been a singleton
     pub owner_chat_id: i64,
     pub secret_token: SecretString,
@@ -10,10 +14,18 @@ pub struct AppState {
 
 impl AppState {
     pub fn new(bot: Bot, owner_chat_id: i64, secret_token: SecretString) -> Self {
-        Self {
+        Self(Arc::new(AppStateInner {
             bot,
             owner_chat_id,
             secret_token,
-        }
+        }))
+    }
+}
+
+impl Deref for AppState {
+    type Target = AppStateInner;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
