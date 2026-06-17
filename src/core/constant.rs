a46cf9c463b8fe1c8968dc8be450345f38aae6e0
saians07 <suberlinsinaga@gmail.com>
2026-06-17 07:14:07 +0700

    Add constant for database connections.

---
+++ src/core/constant.rs
@@ -0,0 +1,7 @@
+// Database Related Constant
+pub const DB_MAX_CONNECTION: u32 = 10;
+pub const DB_MIN_CONNECTION: u32 = 5;
+pub const DB_CONNECTION_TIMEOUT_SECS: u64 = 8;
+pub const DB_ACQUIRE_TIMEOUT_SECS: u64 = 8;
+pub const DB_IDLE_TIMEOUT_SECS: u64 = 8;
+pub const DB_MAX_LIFETIME_SECS: u64 = 8;
