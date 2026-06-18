34a2b74dee9384a05ce0132738b6fd3be88afba9
saians07 <suberlinsinaga@gmail.com>
2026-06-18 11:27:52 +0700

    Remove custom struct to implement chat history creation.

--- src/dto/response.rs
+++ src/dto/response.rs
@@ -1,5 +1,4 @@
 use axum::http::StatusCode;
-use rig::message::Message;
 use sea_orm::{FromJsonQueryResult, FromQueryResult};
 use serde::{Deserialize, Serialize};
 
@@ -8,8 +7,6 @@ pub struct TelegramHistory {
     pub author: String,
     pub message: String,
 }
-
-pub struct TelegramHistoryList(pub Vec<TelegramHistory>);
 
 #[derive(serde::Serialize, utoipa::ToSchema, Debug)]
 pub struct BaseResponse {
@@ -41,62 +38,5 @@ impl BaseResponse {
             message: message.into(),
             data: None,
         }
-    }
-}
-
-impl TelegramHistoryList {
-    pub async fn create_message_vector(self) -> Vec<Message> {
-        let mut messages: String = "".to_string();
-        let mut prev_author: String = "".to_string();
-        let mut history: Vec<Message> = Vec::new();
-        let len_hist = self.0.len() - 1;
-        for (idx, message) in self.0.iter().enumerate() {
-            let message = message.clone();
-            if idx == 0 {
-                messages = message.message.clone();
-                prev_author = "user".to_string();
-                continue;
-            }
-            if idx == len_hist {
-                match message.author.as_str() {
-                    "user" => {
-                        if prev_author == "user" {
-                            messages = format!("{} {}", &messages, &message.message);
-                            history.push(Message::user(messages.clone()));
-                        }
-                        if prev_author == "assistant" {
-                            history.push(Message::user(message.message.clone()));
-                        }
-                    }
-                    "assistant" => {
-                        history.push(Message::assistant(message.message.clone()));
-                    }
-                    _ => {}
-                }
-            }
-            match message.author.as_str() {
-                "user" => {
-                    if prev_author == "user" {
-                        messages = format!("{} {}", &messages, &message.message);
-                    }
-                    if prev_author == "assistant" {
-                        history.push(Message::assistant(messages));
-                        messages = message.message;
-                    }
-                    prev_author = "user".to_string();
-                }
-                "assistant" => {
-                    if prev_author == "user" {
-                        history.push(Message::user(messages));
-                        messages = message.message;
-                        prev_author = "assistant".to_string();
-                    } else {
-                        continue;
-                    }
-                }
-                _ => {}
-            }
-        }
-        history
     }
 }
